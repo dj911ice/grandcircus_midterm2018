dc567fb5aafed87600f33bf218d5ac1c62f3a4af
ZainebYahya <znbyahya@gmail.com>
2018-02-08 17:22:12 -0500

    added validation to RunProgram

--- GCLibrary2018/Program.cs
+++ GCLibrary2018/Program.cs
@@ -18,21 +18,27 @@ namespace GCLibrary2018
 
         public static void RunProgram()
         {
-            
-            Menu.GCLMenu();
-            //Console.WriteLine("Enter Custom Text Here");
-            string Input = Console.ReadLine();
-            if (Input == "1")
-                LibraryApp.PrintBookList(ref BookList);
-            else if (Input == "2")
-                Validation.Search(ref BookList);
-            else if (Input == "3")
-                LibraryApp.CheckOutBook(Validation.ConfirmBook(ref BookList));
-            else if (Input == "4")
-                LibraryApp.ReturnBook(Validation.ConfirmBook(ref BookList));
-            else if (Input == "5")
-
-
+            while (true)
+            {
+                Menu.GCLMenu();
+                //Console.WriteLine("Enter Custom Text Here");
+                string Input = Console.ReadLine();
+                if (Input == "1")
+                    LibraryApp.PrintBookList(ref BookList);
+                else if (Input == "2")
+                    Validation.Search(ref BookList);
+                else if (Input == "3")
+                    LibraryApp.CheckOutBook(Validation.ConfirmBook(ref BookList));
+                else if (Input == "4")
+                    LibraryApp.ReturnBook(Validation.ConfirmBook(ref BookList));
+                else if (Input == "5")
+                    Menu.WriteToFile(ref BookList);
+                else
+                {
+                    Console.WriteLine("That's not an option! Try again!");
+                    Input = Console.ReadLine();
+                }
+            }
             // Code
             // Method calls
         }
