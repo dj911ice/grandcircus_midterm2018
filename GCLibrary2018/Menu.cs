e77e1a4f67fd3b960a32459be7a2970beaaa42c2
Justin Dickerson <justin.p.dickerson@gmail.com>
2018-02-08 18:10:11 +0000

    Added: Menu.cs and Run/Continue methods

---
+++ GCLibrary2018/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCLibrary2018
+{
+    class Menu
+    {
+        public static void GCLMenu ()
+        {
+            Console.WriteLine("Welcome to the Grand Circus Library!\n\n[1] Display Library\n[2] Search Library\n[3] Checkout a Book\n[4] Return a Book\n[5] Exit Library");
+        }
+       
+    }
+}
--- GCLibrary2018/Program.cs
+++ GCLibrary2018/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace GCLibrary2018
 {
@@ -10,6 +11,33 @@ namespace GCLibrary2018
     {
         static void Main(string[] args)
         {
+            RunProgram();
+            ContinueProgram();
+        }
+        public static void RunProgram()
+        {
+            // Menu.GCLMenu(); Prints Menu
+            Console.WriteLine("Enter Custom Text Here");
+            string Input = Console.ReadLine();
+            Console.WriteLine($"Enter Custom Text Here {Input}");
+
+            // Code
+            // Method calls
+        }
+        public static void ContinueProgram()
+        {
+            Console.WriteLine("Enter Custom Text Here? Y/N");
+            string Continue = Console.ReadLine();
+
+            while (Regex.Match(Continue, @"^[Y|y]$").Success)
+            {
+                RunProgram();
+                Continue = "";
+                Console.WriteLine("Enter Custom Text Here? Y/N");
+                Continue = Console.ReadLine();
+            }
+            Console.WriteLine("Enter Custom Text Here");
+            Console.ReadLine();
         }
     }
 }
