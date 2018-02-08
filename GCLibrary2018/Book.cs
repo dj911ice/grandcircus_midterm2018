74c5aab65abdbbad17ca6d59aa4166a6a18e49a3
DanE <edwardsdan001@gmail.com>
2018-02-08 10:08:44 -0500

    added Book class and status enum

---
+++ GCLibrary2018/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCLibrary2018
+{
+
+    class Book
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public DateTime duedate { get; set; }
+        public BookStatus status { get; set; }
+
+        public Book(string Title, string Author, DateTime DueDate, BookStatus Status)
+        {
+            title = Title;
+            author = Author;
+            duedate = DueDate;
+            status = Status;
+        }
+    }
+}
